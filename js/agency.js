59883eaec43096daa765f1ac37bbcd233bb867af
flotob <florian@glatz-mahlberg.de>
2017-02-28 11:13:35 +0100

    disable video background on mobile

--- js/agency.js
+++ js/agency.js
@@ -39,78 +39,83 @@ $('div.modal').on('show.bs.modal', function() {
 /*
 * Youtube Background
 * */
-var tag = document.createElement('script');
-tag.src = 'https://www.youtube.com/player_api';
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-var tv,
-  playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
-var vid = [
-    {'videoId': 'ifhpY1QcFNY', 'startSeconds': 17, 'endSeconds': 303, 'suggestedQuality': 'hd720'},
-    {'videoId': 'KI51NnLBS_Q', 'startSeconds': 0, 'endSeconds': 5, 'suggestedQuality': 'hd720'}
-  ],
-  currVid = 0;
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+  // DO NOT LOAD THE VIDEO BACKGROUND
+} else {
 
-$('.hi em:last-of-type').html(vid.length);
+  var tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/player_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var tv,
+    playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
+  var vid = [
+      {'videoId': 'ifhpY1QcFNY', 'startSeconds': 17, 'endSeconds': 303, 'suggestedQuality': 'hd720'},
+      {'videoId': 'KI51NnLBS_Q', 'startSeconds': 0, 'endSeconds': 5, 'suggestedQuality': 'hd720'}
+    ],
+    currVid = 0;
 
-function onYouTubePlayerAPIReady(){
-  tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
-}
+  $('.hi em:last-of-type').html(vid.length);
 
-function onPlayerReady(){
-  tv.loadVideoById(vid[currVid]);
-  tv.mute();
-}
+  function onYouTubePlayerAPIReady(){
+    tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
+  }
 
-function onPlayerStateChange(e) {
-  if (e.data === 1){
-    $('#tv').addClass('active');
-    $('.hi em:nth-of-type(2)').html(currVid + 1);
-  } else if (e.data === 2){
-    $('#tv').removeClass('active');
-    if(currVid === vid.length - 1){
-      currVid = 0;
-    } else {
-      currVid++;
-    }
+  function onPlayerReady(){
     tv.loadVideoById(vid[currVid]);
-    tv.seekTo(vid[currVid].startSeconds);
-  }
-}
-
-function vidRescale(){
-
-  var w = $(window).width()+200,
-    h = $(window).height()+200;
-
-  if (w/h > 16/9){
-    tv.setSize(w, w/16*9);
-    $('.tv .screen').css({'left': '0px'});
-  } else {
-    tv.setSize(h/9*16, h);
-    $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
-  }
-}
-
-$(window).on('load resize', function(){
-  vidRescale();
-});
-
-$('.hi span:first-of-type').on('click', function(){
-  $('#tv').toggleClass('mute');
-  $('.hi em:first-of-type').toggleClass('hidden');
-  if($('#tv').hasClass('mute')){
     tv.mute();
-  } else {
-    tv.unMute();
   }
-});
 
-$('.hi span:last-of-type').on('click', function(){
-  $('.hi em:nth-of-type(2)').html('~');
-  tv.pauseVideo();
-});
+  function onPlayerStateChange(e) {
+    if (e.data === 1){
+      $('#tv').addClass('active');
+      $('.hi em:nth-of-type(2)').html(currVid + 1);
+    } else if (e.data === 2){
+      $('#tv').removeClass('active');
+      if(currVid === vid.length - 1){
+        currVid = 0;
+      } else {
+        currVid++;
+      }
+      tv.loadVideoById(vid[currVid]);
+      tv.seekTo(vid[currVid].startSeconds);
+    }
+  }
 
+  function vidRescale(){
+
+    var w = $(window).width()+200,
+      h = $(window).height()+200;
+
+    if (w/h > 16/9){
+      tv.setSize(w, w/16*9);
+      $('.tv .screen').css({'left': '0px'});
+    } else {
+      tv.setSize(h/9*16, h);
+      $('.tv .screen').css({'left': -($('.tv .screen').outerWidth()-w)/2});
+    }
+  }
+
+  $(window).on('load resize', function(){
+    vidRescale();
+  });
+
+  $('.hi span:first-of-type').on('click', function(){
+    $('#tv').toggleClass('mute');
+    $('.hi em:first-of-type').toggleClass('hidden');
+    if($('#tv').hasClass('mute')){
+      tv.mute();
+    } else {
+      tv.unMute();
+    }
+  });
+
+  $('.hi span:last-of-type').on('click', function(){
+    $('.hi em:nth-of-type(2)').html('~');
+    tv.pauseVideo();
+  });
+
+}
 
 
 /*
